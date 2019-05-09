a580ebf78d54424b45b85b29ad5e7dffdffb4f7e
TCarmine <kappa85t@hotmail.it>
2019-05-09 18:13:41 +0200

    asyncronous still not working but fixed else for red border

--- memory_Match.js
+++ memory_Match.js
@@ -1,11 +1,13 @@
 let cont = document.getElementById ("gridTable");
 let p = document.getElementById("timer");
+let grid = document.getElementsByClassName("cell");
+
 let interval;
 let started = false;
 let time = 0;
-
+let cell;
 let clickedArray = [];
-
+let answers = [1,1,2,2,3,3,4,4,5];
 let ready = true;
 let numCompleted = 0;
 
@@ -24,17 +26,15 @@ function startTimer(){
 
 
 function randomAnswers(){
-    let answers = [1,1,2,2,3,3,4,4,5];
-    answers.sort(function(item){
-        return .5 - Math.random();
+     answers.sort(function(item){
+        return .6 - Math.random();
     })
     return answers;
-    console.log(answers);
 }
 
 function hide(){
     cell.style.backgroundColor = "blue";
-    cell.innerHTML = "";
+    // cell.innerHTML = "";
     cell.clicked = false;
 }
 function reveal(cell){
@@ -50,56 +50,66 @@ function complete(cell){
 
 function setUp(){
     let answers = randomAnswers();
-    let grid = document.getElementsByClassName("cell");
-    let cell;
-    for(let i=0; i<grid.length; i++){
-        cell = grid[i];
-        cell.completed = false;
-        cell.clicked = false;
-        cell.value = answers[i];
-
-        cell.addEventListener("mouseenter",function(){
-            if(this.completed == false && this.clicked == false)
-               this.style.background = "orange";
-        });
-        cell.addEventListener("mouseleave",function(){
-            if(this.completed == false && this.clicked == false)
-               this.style.background = "blue";
-
-        });
-
-        cell.addEventListener('click',function(){
-            if(ready == false){
-                return;
-            }
-            if(this.clicked == false && this.completed == false){
-                clickedArray.push(this);
-                reveal(this);
-            }
-            startTimer();
-            if(clickedArray.length==2){
-                if(clickedArray[0]==clickedArray[1].value){
-                    complete(clickedArray[0]); 
-                    complete(clickedArray[1]);
-
-                    clickedArray =[];
-
-                    if(numCompleted==8){
-                        alert("You won in "+ time + " seconds!" );
-                        clearInterval(interval);
-                    }
-
-                }else{
-                    ready=false;
-                    cont.style.border = "5px solid red";
-                    setTimeout(function(){
   
-                    },500);
+    
+    for(let i=0; i<grid.length; i++){
+            cell = grid[i];
+            cell.completed = false;
+            cell.clicked = false;
+            cell.value = answers[i];
+
+            cell.addEventListener("mouseenter",function(){
+                if(this.completed == false && this.clicked == false)
+                this.style.background = "orange";
+            });
+            cell.addEventListener("mouseleave",function(){
+                if(this.completed == false && this.clicked == false)
+                this.style.background = "blue";
+
+            });
+
+            cell.addEventListener('click',function(){
+                if(ready == false){
+                    return;
                 }
-                
-            }
-        });
+                if(this.clicked == false && this.completed == false){
+                    clickedArray.push(this);
+                    reveal(this);
+                }
+                startTimer();
+                if(clickedArray.length==2){
+                    if(clickedArray[0]==clickedArray[1].value){
+                        complete(clickedArray[0]); 
+                        complete(clickedArray[1]);
+
+                        clickedArray =[];
+
+                        if(numCompleted==8){
+                            alert("You won in "+ time + " seconds!" );
+                            clearInterval(interval);
+                        }
+
+                    }else{
+                        ready = false;
+                        cont.style.border = "2px solid red";
+                        
+                          
+                        setTimeout(function(){
+                            hide(clickedArray[0]);
+                            hide(clickedArray[1]);
+                            clickedArray = []; 
+                            cont.style.border = "2px solid black";
+                            ready = true;
+                            console.log(i);
+    
+                        },900);
+                        
+                    }
+                    
+                }
+            });
         
+
         
     }
     
