c8c8f14a947113620af60a883f3b8af4c83f3ded
TCarmine <kappa85t@hotmail.it>
2019-05-09 16:06:24 +0200

    some change to the html

--- memory_Match.js
+++ memory_Match.js
@@ -1,8 +1,13 @@
+let cont = document.getElementsByClassName("container");
+
 let interval;
 let started = false;
 let time = 0;
 
 let clickedArray = [];
+
+let ready = true;
+let numCompleted = 0;
 
 setUp();
 
@@ -27,10 +32,20 @@ function randomAnswers(){
     console.log(answers);
 }
 
+function hide(){
+    cell.style.backgroundColor = "blue";
+    cell.innerHTML = "";
+    cell.clicked = false;
+}
 function reveal(cell){
     cell.style.backgroundColor ="red";
     cell.innerHTML = cell.value;
     cell.clicked = true;
+}
+function complete(cell){
+    numCompleted++;
+    cell.completed = true;
+    cell.style.backgroundColor = "purple";
 }
 
 function setUp(){
@@ -53,12 +68,36 @@ function setUp(){
         });
 
         cell.addEventListener('click',function(){
+            if(ready == false){
+                return;
+            }
             if(this.clicked == false && this.completed == false){
                 clickedArray.push(this);
                 reveal(this);
             }
             startTimer();
+            if(clickedArray.length==2){
+                if(clickedArray[0]==clickedArray[1].value){
+                    complete(clickedArray[0]); 
+                    complete(clickedArray[1]);
 
+                    clickedArray =[];
+
+                    if(numCompleted==8){
+                        alert("You won in "+ time + " seconds!" );
+                        clearInterval(interval);
+                    }
+
+                }else{
+
+                    ready=false;
+                    cont.style.border = "5px solid red";
+                    setTimeout(function(){
+  
+                    },500);
+                }
+                
+            }
         });
         
         
