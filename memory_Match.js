75cee28819d503267d2769f82c82ef92eef6a240
TCarmine <kappa85t@hotmail.it>
2019-05-09 11:11:50 +0200

    let version. Not working

---
+++ memory_Match.js
@@ -0,0 +1,33 @@
+let grid = document.getElementsByTagName("div");
+let answers = [1,1,2,2,3,3,4,4,5];
+
+function randomAnswers(){
+   
+    answers.sort(function(item){
+        return .5 - Math.random();
+    })
+    return answers;
+    console.log(answers);
+}
+
+
+function setUp(){
+    console.log(grid);
+    let answers = randomAnswers();
+
+    for(let i=0;i<grid.length;i++){
+        let cell = grid[i];
+        cell.completed = false;
+        cell.clicked = false;
+        cell.value = answers[i];
+    }
+    cell.addEventListener("mouseenter",function(){
+        if(this.completed == false && this.clicked == false)
+           this.style.background = "orange";
+    })
+    cell.addEventListener("mouseleave",function(){
+        if(this.completed == false && this.clicked == false)
+           this.style.background = "blue";
+    })
+
+}
