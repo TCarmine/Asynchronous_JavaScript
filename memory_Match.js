eca1fdcab0dfd863b77c0daa1ae812d3a1076b68
TCarmine <kappa85t@hotmail.it>
2019-05-09 17:13:27 +0200

    fix select container border issue

--- memory_Match.js
+++ memory_Match.js
@@ -1,8 +1,13 @@
+let cont = document.getElementById ("gridTable");
+let p = document.getElementById("timer");
 let interval;
 let started = false;
 let time = 0;
 
 let clickedArray = [];
+
+let ready = true;
+let numCompleted = 0;
 
 setUp();
 
@@ -11,7 +16,7 @@ function startTimer(){
     if(started == false){
         interval = setInterval(function(){
             time++;
-            document.getElementById("timer").innerHTML = "Time Elapsed: " + time;
+            p.innerHTML = "Time Elapsed: " + time;
         },1000)
         started = true;
     }
@@ -27,10 +32,20 @@ function randomAnswers(){
     console.log(answers);
 }
 
+function hide(){
+    cell.style.backgroundColor = "blue";
+    cell.innerHTML = "";
+    cell.clicked = false;
+}
 function reveal(cell){
     cell.style.backgroundColor ="red";
     cell.innerHTML = cell.value;
     cell.clicked = true;
+}
+function complete(cell){
+    numCompleted++;
+    cell.completed = true;
+    cell.style.backgroundColor = "purple";
 }
 
 function setUp(){
@@ -50,15 +65,39 @@ function setUp(){
         cell.addEventListener("mouseleave",function(){
             if(this.completed == false && this.clicked == false)
                this.style.background = "blue";
+
         });
 
         cell.addEventListener('click',function(){
+            if(ready == false){
+                return;
+            }
             if(this.clicked == false && this.completed == false){
                 clickedArray.push(this);
                 reveal(this);
             }
             startTimer();
+            if(clickedArray.length==2){
+                if(clickedArray[0]==clickedArray[1].value){
+                    complete(clickedArray[0]); 
+                    complete(clickedArray[1]);
 
+                    clickedArray =[];
+
+                    if(numCompleted==8){
+                        alert("You won in "+ time + " seconds!" );
+                        clearInterval(interval);
+                    }
+
+                }else{
+                    ready=false;
+                    cont.style.border = "5px solid red";
+                    setTimeout(function(){
+  
+                    },500);
+                }
+                
+            }
         });
         
         
